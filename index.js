b53e06bb5e47914d4124bc3e302926c6bf992846
ccboys <walter@intrising.com.tw>
2021-08-03 10:14:26 +0800

    fix: fix typo again

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function (opts) {
   var server = new Server(opts)
 
   return {
-    handleQuery: registry.handleQuery.bind(null, query),
+    handleQuery: registry.handleQuery.bind(null, server),
     setText: registry.setText.bind(null, server),
     publish: registry.publish.bind(null, server),
     unpublishAll: registry.unpublishAll.bind(null, server),
